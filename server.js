9e902a7484d6504f8e6f3d28acdd1b58d93c506d
606682 <marcusstancil@C02YJ0QPJGH7.fios-router.home>
2020-03-04 10:43:33 -0500

    fix routing

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
 var app = express()
+const history = require('connect-history-api-fallback')
+app.use(serveStatic(path.join(__dirname, '/dist')))
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+   }))
 app.use(serveStatic(path.join(__dirname, '/dist')))
 var port = process.env.PORT || 5000
 app.listen(port)
